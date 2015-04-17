0fb113348bce7ce119c05a85052c4a0363a524dd
parkermc <nick@reisingerfamily.net>
2015-04-17 10:50:36 -0500

    made start.py

---
+++ start.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      SchoolWInternet
+#
+# Created:     17/04/2015
+# Copyright:   (c) SchoolWInternet 2015
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
