be06a8e85c4afb0a59f2c51f9b7956fd7372b486
parkermc <nick@reisingerfamily.net>
2015-04-18 16:49:41 -0500

    add file open

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-import sys
+import sys, wx, file
 from PyQt4 import QtCore, QtGui, uic
 
 form_class = uic.loadUiType("ui/opening.ui")[0]
@@ -7,6 +7,14 @@ class MyWindowClass(QtGui.QMainWindow, form_class):
     def __init__(self, parent=None):
         QtGui.QMainWindow.__init__(self, parent)
         self.setupUi(self)
+        self.open.clicked.connect(self.openc)
+        self.newc.clicked.connect(self.new)
+
+    def openc(self):
+        cfile = file.openf()
+        self.close()
+    def new(self):
+        self.close()
 
 app = QtGui.QApplication(sys.argv)
 myWindow = MyWindowClass()
