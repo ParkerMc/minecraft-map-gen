196aa9fdd3834574d477b8c9a6bec9d45b231875
parkermc <nick@reisingerfamily.net>
2015-04-27 21:55:22 -0500

    added get os and the bit

--- start.py
+++ start.py
@@ -1,6 +1,16 @@
 import sys, wx, file
 from PyQt4 import QtCore, QtGui, uic
 
+if sys.platform == "win32":
+    global ops
+    ops = "32"
+elif sys.platform == "win64"
+    global ops
+    ops = "64"
+elif sys.platform != "win64" and sys.platform != "win32":
+    global ops
+    ops = "outher"
+
 form_class = uic.loadUiType("ui/opening.ui")[0]
 
 class MyWindowClass(QtGui.QMainWindow, form_class):
