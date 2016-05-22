830d9e7b0d82da215a499828c703dab48fd7a980
ParkerMc <nick@reisingerfamily.net>
2016-05-21 21:45:37 -0500

    moved splach to start.pyw and changed build.py

--- build.py
+++ build.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 import py2exe, sys, os
 
-Mydata_files = [('', ['os']),('assets',['assets/splach.png'])]
+Mydata_files = [('', ['os']),('assets',['assets/splach.png','assets/icon.ico']),('ui',['ui/main.ui'])]
 sys.argv.append('py2exe')
 
 setup(
     data_files = Mydata_files,
     options = {'py2exe': {'bundle_files': 1, 'compressed': True,"dll_excludes":['w9xpopen.exe']}},
-    windows = [{"icon_resources": [(1, "assets/icon.ico")],'script': 'start.py'}],
+    windows = [{"icon_resources": [(1, "F:/git/Map/assets/icon.ico")],'script': 'start.pyw'}],
     zipfile = None,
 )
